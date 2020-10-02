644bef80395e25f3ca46e9ac59fe41c806e5d57c
johnabass <johnbass1017@yahoo.com>
2020-10-02 15:59:56 -0700

    implement mapstructure hooks for all the zapcore.EncoderConfig types

--- decode.go
+++ decode.go
@@ -7,6 +7,8 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+var stringType = reflect.TypeOf("")
+
 var (
 	zapcoreLevelType      = reflect.TypeOf(zapcore.Level(0))
 	zapcoreLevelPtrType   = reflect.PtrTo(zapcoreLevelType)
@@ -14,7 +16,7 @@ var (
 	zapAtomicLevelPtrType = reflect.PtrTo(zapAtomicLevelType)
 )
 
-// StringToLevelHookFunc is a mapstructure DecodeHookFuncType implementation
+// StringToLevelHook is a mapstructure DecodeHookFuncType implementation
 // that handles converting from a string to a zap logging level.  This function will
 // convert to any of the following types, based on the to parameter:
 //
@@ -42,29 +44,99 @@ var (
 // This function is necessary because libraries like spf13/viper do not directly unmarshal
 // into types.  Rather, they unmarshal first to a neutral format, e.g. map[string]interface{},
 // and then use mapstructure to take that format and convert it into a struct.
-func StringToLevelHookFunc(from, to reflect.Type, src interface{}) (interface{}, error) {
-	if text, ok := src.(string); ok {
-		switch {
-		case to == zapcoreLevelType:
+func StringToLevelHook(from, to reflect.Type, src interface{}) (interface{}, error) {
+	if from == stringType {
+		switch to {
+		case zapcoreLevelType:
 			var l zapcore.Level
-			err := l.UnmarshalText([]byte(text))
+			err := l.UnmarshalText([]byte(src.(string)))
 			return l, err
 
-		case to == zapcoreLevelPtrType:
+		case zapcoreLevelPtrType:
 			l := new(zapcore.Level)
-			err := l.UnmarshalText([]byte(text))
+			err := l.UnmarshalText([]byte(src.(string)))
 			return l, err
 
-		case to == zapAtomicLevelType:
+		case zapAtomicLevelType:
 			l := zap.NewAtomicLevel()
-			err := l.UnmarshalText([]byte(text))
+			err := l.UnmarshalText([]byte(src.(string)))
 			return l, err
 
-		case to == zapAtomicLevelPtrType:
+		case zapAtomicLevelPtrType:
 			l := zap.NewAtomicLevel()
-			err := l.UnmarshalText([]byte(text))
+			err := l.UnmarshalText([]byte(src.(string)))
 			return &l, err
 		}
+	}
+
+	return src, nil
+}
+
+var levelEncoderType = reflect.TypeOf(zapcore.LevelEncoder(nil))
+
+// StringToLevelEncoderHook is a mapstructure DecodeHookFuncType that converts from a string
+// to a zapcore.LevelEncoder via the zapcore.LevelEncoder.UnmarshalText method.
+func StringToLevelEncoderHook(from, to reflect.Type, src interface{}) (interface{}, error) {
+	if from == stringType && to == levelEncoderType {
+		var le zapcore.LevelEncoder
+		err := le.UnmarshalText([]byte(src.(string)))
+		return le, err
+	}
+
+	return src, nil
+}
+
+var timeEncoderType = reflect.TypeOf(zapcore.TimeEncoder(nil))
+
+// StringToTimeEncoderHook is a mapstructure DecodeHookFuncType that converts from a string
+// to a zapcore.TimeEncoder via the zapcore.TimeEncoder.UnmarshalText method.
+func StringToTimeEncoderHook(from, to reflect.Type, src interface{}) (interface{}, error) {
+	if from == stringType && to == timeEncoderType {
+		var te zapcore.TimeEncoder
+		err := te.UnmarshalText([]byte(src.(string)))
+		return te, err
+	}
+
+	return src, nil
+}
+
+var durationEncoderType = reflect.TypeOf(zapcore.DurationEncoder(nil))
+
+// StringToDurationEncoderHook is a mapstructure DecodeHookFuncType that converts from a string
+// to a zapcore.DurationEncoder via the zapcore.DurationEncoder.UnmarshalText method.
+func StringToDurationEncoderHook(from, to reflect.Type, src interface{}) (interface{}, error) {
+	if from == stringType && to == durationEncoderType {
+		var de zapcore.DurationEncoder
+		err := de.UnmarshalText([]byte(src.(string)))
+		return de, err
+	}
+
+	return src, nil
+}
+
+var callerEncoderType = reflect.TypeOf(zapcore.CallerEncoder(nil))
+
+// StringToCallerEncoderHook is a mapstructure DecodeHookFuncType that converts from a string
+// to a zapcore.CallerEncoder via the zapcore.CallerEncoder.UnmarshalText method.
+func StringToCallerEncoderHook(from, to reflect.Type, src interface{}) (interface{}, error) {
+	if from == stringType && to == callerEncoderType {
+		var ce zapcore.CallerEncoder
+		err := ce.UnmarshalText([]byte(src.(string)))
+		return ce, err
+	}
+
+	return src, nil
+}
+
+var nameEncoderType = reflect.TypeOf(zapcore.NameEncoder(nil))
+
+// StringToNameEncoderHook is a mapstructure DecodeHookFuncType that converts from a string
+// to a zapcore.NameEncoder via the zapcore.NameEncoder.UnmarshalText method.
+func StringToNameEncoderHook(from, to reflect.Type, src interface{}) (interface{}, error) {
+	if from == stringType && to == nameEncoderType {
+		var ne zapcore.NameEncoder
+		err := ne.UnmarshalText([]byte(src.(string)))
+		return ne, err
 	}
 
 	return src, nil
--- decode_test.go
+++ decode_test.go
@@ -1,74 +1,351 @@
 package sallust
 
 import (
+	"bytes"
 	"reflect"
-	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-func TestStringToLevelHookFunc(t *testing.T) {
-	expectedZapcoreLevel := zapcore.DebugLevel
-	expectedZapLevel := zap.NewAtomicLevelAt(zapcore.DebugLevel)
+func testStringToLevelHookNoConversion(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		result, err = StringToLevelHook(
+			reflect.TypeOf(int(0)),
+			reflect.TypeOf(""),
+			123,
+		)
+	)
 
-	testData := []struct {
-		from  reflect.Type
-		to    reflect.Type
-		value interface{}
+	assert.Equal(123, result)
+	assert.NoError(err)
+}
 
-		expected    interface{}
-		expectedErr error
-	}{
-		{
-			from:     reflect.TypeOf(int(0)),
-			to:       reflect.TypeOf(""),
-			value:    123,
-			expected: 123,
-		},
-		{
-			from:     reflect.TypeOf(""),
-			to:       reflect.TypeOf(float64(0.0)),
-			value:    "test",
-			expected: "test",
-		},
-		{
-			from:     reflect.TypeOf(""),
-			to:       reflect.TypeOf(zapcore.Level(0)),
-			value:    "debug",
-			expected: zapcore.DebugLevel,
-		},
-		{
-			from:     reflect.TypeOf(""),
-			to:       reflect.PtrTo(reflect.TypeOf(zapcore.Level(0))),
-			value:    "debug",
-			expected: &expectedZapcoreLevel,
-		},
-		{
-			from:     reflect.TypeOf(""),
-			to:       reflect.TypeOf(zap.AtomicLevel{}),
-			value:    "debug",
-			expected: expectedZapLevel,
-		},
-		{
-			from:     reflect.TypeOf(""),
-			to:       reflect.PtrTo(reflect.TypeOf(zap.AtomicLevel{})),
-			value:    "debug",
-			expected: &expectedZapLevel,
-		},
-	}
+func testStringToLevelHookToZapcoreLevel(t *testing.T) {
+	var (
+		assert   = assert.New(t)
+		expected = zapcore.DebugLevel
+	)
 
-	for i, record := range testData {
-		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			var (
-				assert            = assert.New(t)
-				actual, actualErr = StringToLevelHookFunc(record.from, record.to, record.value)
-			)
+	result, err := StringToLevelHook(
+		reflect.TypeOf(""),
+		reflect.TypeOf(zapcore.Level(0)),
+		"debug",
+	)
 
-			assert.Equal(record.expected, actual)
-			assert.Equal(record.expectedErr, actualErr)
-		})
-	}
+	assert.Equal(expected, result)
+	assert.NoError(err)
+
+	result, err = StringToLevelHook(
+		reflect.TypeOf(""),
+		reflect.TypeOf(new(zapcore.Level)),
+		"debug",
+	)
+
+	assert.Equal(&expected, result)
+	assert.NoError(err)
+}
+
+func testStringToLevelHookToZapAtomicLevel(t *testing.T) {
+	var (
+		assert   = assert.New(t)
+		expected = zap.NewAtomicLevelAt(zapcore.DebugLevel)
+	)
+
+	result, err := StringToLevelHook(
+		reflect.TypeOf(""),
+		reflect.TypeOf(zap.AtomicLevel{}),
+		"debug",
+	)
+
+	assert.Equal(expected, result)
+	assert.NoError(err)
+
+	result, err = StringToLevelHook(
+		reflect.TypeOf(""),
+		reflect.TypeOf(new(zap.AtomicLevel)),
+		"debug",
+	)
+
+	assert.Equal(&expected, result)
+	assert.NoError(err)
+}
+
+func TestStringToLevelHook(t *testing.T) {
+	t.Run("NoConversion", testStringToLevelHookNoConversion)
+	t.Run("ToZapcoreLevel", testStringToLevelHookToZapcoreLevel)
+	t.Run("ToZapAtomicLevel", testStringToLevelHookToZapAtomicLevel)
+}
+
+func testStringToLevelEncoderHookNoConversion(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		result, err = StringToLevelEncoderHook(
+			reflect.TypeOf(int(0)),
+			reflect.TypeOf(""),
+			123,
+		)
+	)
+
+	assert.Equal(123, result)
+	assert.NoError(err)
+}
+
+func testStringToLevelEncoderHookSuccess(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		require     = require.New(t)
+		result, err = StringToLevelEncoderHook(
+			reflect.TypeOf(""),
+			reflect.TypeOf(zapcore.LevelEncoder(nil)),
+			"capital",
+		)
+	)
+
+	assert.NoError(err)
+
+	levelEncoder, ok := result.(zapcore.LevelEncoder)
+	require.True(ok)
+
+	var output bytes.Buffer
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+			LevelKey:    "level",
+			EncodeLevel: levelEncoder,
+		}),
+		zapcore.AddSync(&output),
+		zapcore.DebugLevel,
+	)
+
+	core.Write(zapcore.Entry{
+		Level: zapcore.InfoLevel,
+	}, nil)
+
+	assert.Contains(output.String(), "INFO")
+}
+
+func TestStringToLevelEncoderHook(t *testing.T) {
+	t.Run("NoConversion", testStringToLevelEncoderHookNoConversion)
+	t.Run("Success", testStringToLevelEncoderHookSuccess)
+}
+
+func testStringToTimeEncoderHookNoConversion(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		result, err = StringToTimeEncoderHook(
+			reflect.TypeOf(int(0)),
+			reflect.TypeOf(""),
+			123,
+		)
+	)
+
+	assert.Equal(123, result)
+	assert.NoError(err)
+}
+
+func testStringToTimeEncoderHookSuccess(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		require     = require.New(t)
+		now         = time.Now()
+		result, err = StringToTimeEncoderHook(
+			reflect.TypeOf(""),
+			reflect.TypeOf(zapcore.TimeEncoder(nil)),
+			"RFC3339",
+		)
+	)
+
+	assert.NoError(err)
+
+	timeEncoder, ok := result.(zapcore.TimeEncoder)
+	require.True(ok)
+
+	var output bytes.Buffer
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+			TimeKey:    "ts",
+			EncodeTime: timeEncoder,
+		}),
+		zapcore.AddSync(&output),
+		zapcore.DebugLevel,
+	)
+
+	core.Write(zapcore.Entry{
+		Time: now,
+	}, nil)
+
+	assert.Contains(output.String(), now.Format(time.RFC3339))
+}
+
+func TestStringToTimeEncoderHook(t *testing.T) {
+	t.Run("NoConversion", testStringToTimeEncoderHookNoConversion)
+	t.Run("Success", testStringToTimeEncoderHookSuccess)
+}
+
+func testStringToDurationEncoderHookNoConversion(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		result, err = StringToDurationEncoderHook(
+			reflect.TypeOf(int(0)),
+			reflect.TypeOf(""),
+			123,
+		)
+	)
+
+	assert.Equal(123, result)
+	assert.NoError(err)
+}
+
+func testStringToDurationEncoderHookSuccess(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		require     = require.New(t)
+		result, err = StringToDurationEncoderHook(
+			reflect.TypeOf(""),
+			reflect.TypeOf(zapcore.DurationEncoder(nil)),
+			"string",
+		)
+	)
+
+	assert.NoError(err)
+
+	durationEncoder, ok := result.(zapcore.DurationEncoder)
+	require.True(ok)
+
+	var output bytes.Buffer
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+			EncodeDuration: durationEncoder,
+		}),
+		zapcore.AddSync(&output),
+		zapcore.DebugLevel,
+	)
+
+	core.Write(zapcore.Entry{}, []zapcore.Field{
+		{
+			Key:     "test",
+			Type:    zapcore.DurationType,
+			Integer: int64(17 * time.Minute),
+		},
+	})
+
+	assert.Contains(output.String(), "17m")
+}
+
+func TestStringToDurationEncoderHook(t *testing.T) {
+	t.Run("NoConversion", testStringToDurationEncoderHookNoConversion)
+	t.Run("Success", testStringToDurationEncoderHookSuccess)
+}
+
+func testStringToCallerEncoderHookNoConversion(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		result, err = StringToCallerEncoderHook(
+			reflect.TypeOf(int(0)),
+			reflect.TypeOf(""),
+			123,
+		)
+	)
+
+	assert.Equal(123, result)
+	assert.NoError(err)
+}
+
+func testStringToCallerEncoderHookSuccess(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		require     = require.New(t)
+		result, err = StringToCallerEncoderHook(
+			reflect.TypeOf(""),
+			reflect.TypeOf(zapcore.CallerEncoder(nil)),
+			"short",
+		)
+	)
+
+	assert.NoError(err)
+
+	callerEncoder, ok := result.(zapcore.CallerEncoder)
+	require.True(ok)
+
+	var output bytes.Buffer
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+			CallerKey:    "test",
+			EncodeCaller: callerEncoder,
+		}),
+		zapcore.AddSync(&output),
+		zapcore.DebugLevel,
+	)
+
+	core.Write(zapcore.Entry{
+		Caller: zapcore.EntryCaller{
+			Defined: true,
+			File:    "foo/bar.go",
+			Line:    123,
+		},
+	}, nil)
+
+	assert.Contains(output.String(), "foo/bar.go:123")
+}
+
+func TestStringToCallerEncoderHook(t *testing.T) {
+	t.Run("NoConversion", testStringToCallerEncoderHookNoConversion)
+	t.Run("Success", testStringToCallerEncoderHookSuccess)
+}
+
+func testStringToNameEncoderHookNoConversion(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		result, err = StringToNameEncoderHook(
+			reflect.TypeOf(int(0)),
+			reflect.TypeOf(""),
+			123,
+		)
+	)
+
+	assert.Equal(123, result)
+	assert.NoError(err)
+}
+
+func testStringToNameEncoderHookSuccess(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		require     = require.New(t)
+		result, err = StringToNameEncoderHook(
+			reflect.TypeOf(""),
+			reflect.TypeOf(zapcore.NameEncoder(nil)),
+			"full",
+		)
+	)
+
+	assert.NoError(err)
+
+	nameEncoder, ok := result.(zapcore.NameEncoder)
+	require.True(ok)
+
+	var output bytes.Buffer
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+			NameKey:    "test",
+			EncodeName: nameEncoder,
+		}),
+		zapcore.AddSync(&output),
+		zapcore.DebugLevel,
+	)
+
+	core.Write(zapcore.Entry{
+		LoggerName: "foo.bar",
+	}, nil)
+
+	assert.Contains(output.String(), "foo.bar")
+}
+
+func TestStringToNameEncoderHook(t *testing.T) {
+	t.Run("NoConversion", testStringToNameEncoderHookNoConversion)
+	t.Run("Success", testStringToNameEncoderHookSuccess)
 }
