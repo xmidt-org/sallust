4dcd1e8435122c2aca49b41439dd55dfcdc45296
Owen Cabalceta <denopink@gmail.com>
2022-10-19 12:49:23 -0400

    Update enrich_test.go

--- enrich_test.go
+++ enrich_test.go
@@ -3,7 +3,11 @@ package sallust
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest"
 )
 
 type testContextual struct {
@@ -14,35 +18,40 @@ func (tc testContextual) Metadata() map[string]interface{} {
 	return map[string]interface{}{tc.key: tc.value}
 }
 
-func testEnrichNoObjects(t *testing.T) {
-	var (
-		require  = require.New(t)
-		delegate = new(mockLogger)
-	)
-
-	logger := Enrich(delegate)
-	require.NotNil(logger)
-
-	delegate.On("Log", []interface{}{"message", "foobar"}).Return(error(nil)).Once()
-	logger.Log("message", "foobar")
-	delegate.AssertExpectations(t)
-}
-
-func testEnrichWithObjects(t *testing.T) {
-	var (
-		require  = require.New(t)
-		delegate = new(mockLogger)
-	)
-
-	logger := Enrich(delegate, map[string]string{"key1": "value1"}, nil, map[string]interface{}{"key2": "value2"}, 27, testContextual{"key3", "value3"})
-	require.NotNil(logger)
-
-	delegate.On("Log", []interface{}{"key1", "value1", "key2", "value2", "key3", "value3", "message", "foobar"}).Return(error(nil)).Once()
-	logger.Log("message", "foobar")
-	delegate.AssertExpectations(t)
-}
-
 func TestEnrich(t *testing.T) {
-	t.Run("NoObjects", testEnrichNoObjects)
-	t.Run("WithObjects", testEnrichWithObjects)
+	tests := []struct {
+		description string
+		objects     []interface{}
+		expectedErr error
+	}{
+		{
+			description: "Validate enrich logging without objects",
+		},
+		{
+			description: "Validate enrich logging with objects",
+			objects:     []interface{}{"key1", "value1", "key2", "value2", "key3", "value3", "message", "foobar"},
+		},
+		{
+			description: "Validate enrich logging with objects",
+			objects:     []interface{}{[]interface{}{"key1", "value1", "message", "foobar"}, 27, testContextual{"key3", "value3"}},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			assert := assert.New(t)
+			require := require.New(t)
+			logCount := 0
+			logger := zaptest.NewLogger(t, zaptest.WrapOptions(zap.Hooks(
+				func(e zapcore.Entry) error {
+					logCount++
+					return nil
+				})))
+			logger = Enrich(logger, tc.objects...)
+
+			require.NotNil(logger)
+			logger.Info("foobar")
+			assert.Equal(1, logCount)
+		})
+	}
 }
