2f119deb05286fe5854343eaa1a59128c4906f28
Owen Cabalceta <denopink@gmail.com>
2022-10-21 14:39:58 -0400

    no longer needed

--- log.go
+++ log.go
@@ -5,30 +5,15 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"runtime"
-	"strconv"
-	"strings"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-// A Valuer generates a log value. When passed to With
-// in a value element (odd indexes), it represents a dynamic
-// value which is re-evaluated with each log event.
-type Valuer func() interface{}
-
 // LoggerFunc is a strategy for adding key/value pairs (possibly) based on an HTTP request.
 // Functions of this type must append key/value pairs to the supplied slice and then return
 // the new slice.
 type LoggerFunc func([]zap.Field, *http.Request) []zap.Field
-
-var (
-
-	// DefaultCaller is a Valuer that returns the file and line where the Log
-	// method was invoked. It can only be used with log.With.
-	DefaultCaller = Caller(3)
-)
 
 func NewTestLogger() (*bytes.Buffer, *zap.Logger) {
 	b := &bytes.Buffer{}
@@ -50,17 +35,4 @@ func NewServerLogger(serverName string, logger *zap.Logger) *log.Logger {
 		serverName,
 		log.LstdFlags|log.LUTC,
 	)
-}
-
-// Caller returns a Valuer that returns a file and line from a specified depth
-// in the callstack. Users will probably want to use DefaultCaller.
-func Caller(depth int) Valuer {
-	return func() interface{} {
-		_, file, line, _ := runtime.Caller(depth)
-		idx := strings.LastIndexByte(file, '/')
-		// using idx+1 below handles both of following cases:
-		// idx == -1 because no "/" was found, or
-		// idx >= 0 and we want to start at the character after the found "/".
-		return file[idx+1:] + ":" + strconv.Itoa(line)
-	}
 }
