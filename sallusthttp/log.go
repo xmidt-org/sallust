b85c8413feb0b323f544847c07291d98c18eb2ea
Owen Cabalceta <denopink@gmail.com>
2022-11-02 16:40:27 -0400

    Update log.go

--- sallusthttp/log.go
+++ sallusthttp/log.go
@@ -13,8 +13,8 @@ func NewConnStateLogger(logger *zap.Logger, lvl zapcore.Level, fs ...zap.Field) 
 		fs = append(fs, zap.String("connState", cs.String()))
 		if addr := c.LocalAddr(); addr != nil {
 			fs = append(fs, zap.String("localAddress", addr.String()))
-
 		}
+
 		logger.Log(
 			lvl,
 			"connState",
