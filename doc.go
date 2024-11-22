326ddc76e044940f2ea7f019d65928ea4fbb7cae
johnabass <johnbass1017@yahoo.com>
2024-11-22 11:33:13 -0800

    added copyright notices

--- doc.go
+++ doc.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Comcast Cable Communications Management, LLC
+// SPDX-License-Identifier: Apache-2.0
+
 /*
 Package sallust adds a few useful features around the go.uber.org/zap package:
 
